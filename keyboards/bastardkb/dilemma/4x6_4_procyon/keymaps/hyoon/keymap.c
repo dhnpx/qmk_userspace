4695d3ff1fcd85e6e9435e4a2ca104c060ba112e
hyoon <122553352+dhnpx@users.noreply.github.com>
2026-04-21 11:42:43 -0700

    removed const

--- keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/hyoon/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/hyoon/keymap.c
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 #ifdef COMBO_ENABLE
-const combo_t key_combos[] = {};
+combo_t key_combos[] = {};
 #endif
 
 // clang-format on
