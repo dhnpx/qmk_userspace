1962fd2dcce5438fb3552b1880d30d3562c028d5
hyoon <122553352+dhnpx@users.noreply.github.com>
2026-05-12 03:17:13 -0700

    changed symf

--- keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/hyoon/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/hyoon/keymap.c
@@ -65,9 +65,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_MINS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_LSFT,    KC_A,    KC_S,   KC_D,   SYM_F,   KC_G,         KC_H,    KC_J,     KC_K,   KC_L, KC_SCLN, KC_RSFT,
+       KC_LSFT,    KC_A,    KC_S,   KC_D,   KC_F,   KC_G,         KC_H,    KC_J,     KC_K,   KC_L, KC_SCLN, KC_RSFT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_LCTL,    PT_Z,    KC_X,    KC_C,    CTL_V,   KC_B,       KC_N,   CTL_M, KC_COMM,  KC_DOT, PT_SLSH, KC_RCTL,
+       KC_LCTL,    PT_Z,    KC_X,    KC_C,  KC_V,   KC_B,        KC_N,   KC_M, KC_COMM,  KC_DOT, PT_SLSH, KC_RCTL,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                          KC_LALT, KC_LGUI,  SYM_SPC,   LOWER,      RAISE,  SYM_ENT, KC_ESC,  KC_DEL
   //                    ╰───────────────────────────────────╯ ╰───────────────────────────────────╯
